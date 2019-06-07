9fdaa72f4232a4825f6927ae070514e720d5361c
XtremePineapple <47584029+XtremePineapple@users.noreply.github.com>
2019-06-06 18:59:51 -0700

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
